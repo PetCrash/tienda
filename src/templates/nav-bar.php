fb681f124cca8b487ca1aabcb415b3f652f635b9
PetCrash <pedro.luis.cerpa@gmail.com>
2025-01-19 02:00:30 +0000

    Arreglo no se cargaban todas las opciones del menu

--- src/templates/nav-bar.php
+++ src/templates/nav-bar.php
@@ -6,11 +6,16 @@ if (!empty($_GET))
 else {
     $page = "Calzado";
 }
+
+// Consulta para saber las clases diferentes que hay
+$query = "SELECT DISTINCT clase FROM producto";
+$result = $dbConn->query($query);
 $clases = array();
-foreach($products as $product) {
-    array_push($clases, $product['clase']);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        array_push($clases, $row["clase"]);
+    }
 }
-$clases = array_unique($clases);
 ?>
 <div class="container-nav-var">
     <div class="menu">
